9a73fd8c831c664ee1acd147e56e23a8ce201a40
Vivek Baruah <vivekbrh01@gmail.com>
2023-01-27 14:45:38 +0530

    Update membership slider page

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {LogBox} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import RNBootSplash from 'react-native-bootsplash';
 
 import Home from './src/components/Home';
 
 function App(): JSX.Element {
   const Stack = createNativeStackNavigator();
+  RNBootSplash.hide({fade: true});
+  useEffect(() => {
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+  }, []);
 
   return (
     <NavigationContainer>
---
+++ src/utils/ImagesLinks.js
@@ -0,0 +1,9 @@
+const ImageLinks = {
+  // Home
+  homeBackground: require('../assets/images/home/home-background.png'),
+  // arrows
+  backArrowWhite: require('../assets/images/arrow/back-arrow-white.png'),
+  rightArrowWhite: require('../assets/images/arrow/right-arrow-white.png'),
+};
+
+export default ImageLinks;
